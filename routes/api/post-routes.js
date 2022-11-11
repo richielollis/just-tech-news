709f7338392658a2d211f1c562d083329f3d80ee
richielollis <richiedlollis@gmail.com>
2022-11-11 15:30:30 -0500

    comment route and model completed and code refactor

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-const { Post, User, Vote, Comment } = require("../../models");
+const { Post, User, Comment, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
-    order: [["created_at", "DESC"]],
     attributes: [
       "id",
       "post_url",
@@ -19,8 +18,8 @@ router.get("/", (req, res) => {
         "vote_count",
       ],
     ],
+    order: [["created_at", "DESC"]],
     include: [
-      // include the Comment model here:
       {
         model: Comment,
         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
@@ -61,6 +60,14 @@ router.get("/:id", (req, res) => {
     ],
     include: [
       {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
         model: User,
         attributes: ["username"],
       },
@@ -95,11 +102,11 @@ router.post("/", (req, res) => {
 
 router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then((updatedPostData) => res.json(updatedPostData))
+  Post.upvote(req.body, { Vote, Comment, User })
+    .then((updatedVoteData) => res.json(updatedVoteData))
     .catch((err) => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
 });
 
